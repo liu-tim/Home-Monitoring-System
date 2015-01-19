a3bd1d914660b3b120982c5ab2ea5357f81c8ae2
Tim Liu <ht2liu@uwaterloo.ca>
2015-01-18 19:31:12 -0500

    Update Blackout_Email_Notifier.ino

--- Blackout_Email_Notifier.ino
+++ Blackout_Email_Notifier.ino
@@ -3,32 +3,31 @@ int minSecsBetweenEmails = 60; // 1 min
 long lastSend = -minSecsBetweenEmails * 1000l;
 void setup()
 {
-   for(i=2;i<5;i++)
- {
-  pinMode(i, OUTPUT);  
- }   
-Serial.begin(9600);
+  for(i=2;i<5;i++)
+   {
+     pinMode(i, OUTPUT);  
+   }  
+ Serial.begin(9600);
 }
+
 void loop()
 {
-long now = millis();
-if (analogRead(A3)>500)
-{
-  for(i=2;i<5;i++)
+  long now = millis();
+  if (analogRead(A3)>500)
+  {
+     for(i=2;i<5;i++)
      {
-      digitalWrite(i, HIGH);
-     }
-  if (now > (lastSend + minSecsBetweenEmails * 1000l))
-  {
-  Serial.println("Sending Email!");
-  lastSend = now;
+      digitalWrite(i, HIGH);  
+     }   
+    if (now > (lastSend + minSecsBetweenEmails * 1000l))
+    {
+      Serial.println("Lights Are Turned Off!");
+      lastSend = now;
+    }
+    else
+    {
+      Serial.println("Email Sent...Turning On Emergency Blackout Lights");
+    }
   }
-  else
-  {
-  Serial.println("Wait");
-  }
+  delay(500);
 }
-
-delay(500);
-}
-
